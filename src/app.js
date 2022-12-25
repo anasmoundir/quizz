f5fd601cdc7874d670a3c5a6dc95e3b43c214af9
anasmoundir <anasmoundir1996@gmail.com>
2022-12-25 17:19:05 +0100

    js

--- src/app.js
+++ src/app.js
@@ -110,114 +110,146 @@ var questions =  [
           "the explication":"The AWS Acceptable Use Policy provides information regarding prohibited actions on the AWS infrastructure."
         }
     ]
-
-
 const step= document.querySelector('.title-compo');
-
 const step2= document.getElementById('spacediv2');
 let  button = document.getElementById('btn');
 let buttondata = document.getElementById('btn-test');
 let questiontitle = document.getElementById('questiontitle');
 var part = document.getElementById('displayQA').style.display ="none"; 
+document.getElementById('displayQA2').style.display = "none"
+document.getElementById('displayQA3').style.display = "none"
 answerquestion1= document.getElementById('cb1');
 answerquestion2 = document.getElementById('cb2');
 answerquestion3 = document.getElementById('cb3');
 answerquestion4 = document.getElementById('cb4');
 valuecheck1 = document.getElementById('cb11');
-valuecheck2 = document.getElementById('cb21')
-valuecheck3 = document.getElementById('cb31');
-valuecheck4 = document.getElementById('cb41');
-
+ valuecheck2 = document.getElementById('cb21') 
+ valuecheck3 = document.getElementById('cb31');
+ valuecheck4 = document.getElementById('cb41');
+var  correct = 0;
 function reload()
 {
       window.location.reload();
 }
-
 function arraygenratore()
 {
   randArr =[];
   while(randArr.length < 10){
     val = Math.floor((Math.random() * 10));
-  
     if(randArr.indexOf(val) < 0){
       randArr.push(val);
     }
   }
   return randArr;
 }
-var random = arraygenratore();
 
+var random = arraygenratore();
 function* generator() {
   let toYield = random;
   for (let i=0; i<toYield.length-1; i++) yield toYield[i];
   return toYield.pop(); 
 }
+
 var ran = generator();
+var rna1;
+var compare;
+var correctquestion = 0;
+var arr = [];
 function passTheNextquestion()
 {
+  initilizethe();
   let rna1 = ran.next();
-  if(rna1.length == 11)
+  StepForward(rna1);
+  if(rna1.done)
   {
-     alert('this is the finale question')
+    document.getElementById('displayQA').style.display ="none"; 
+    document.getElementById('spacediv3').style.backgroundColor = 'green'
+    document.getElementById('displayQA3').style.display = "block"
+    var resultat = document.getElementById('result').innerHTML ="YOUR CORRECT ANSWERS ARE"+ " " +correctquestion +"/10";
   }
- 
   questiontitle.textContent = questions[rna1.value].question;
   answerquestion1.textContent = questions[rna1.value].answers[0]
-  answerquestion2.textContent =questions[rna1.value].answers[1]
-  answerquestion3.textContent =questions[rna1.value].answers[2]
-  answerquestion4.textContent =questions[rna1.value].answers[3]
-  valuecheck1.value =  questions[rna1.value].answers[0]
-  valuecheck2.value =  questions[rna1.value].answers[1]
-  valuecheck3.value =  questions[rna1.value].answers[2]
-  valuecheck4.value =  questions[rna1.value].answers[3]
-
-  console.log(rna1.value);
+  answerquestion2.textContent = questions[rna1.value].answers[1]
+  answerquestion3.textContent = questions[rna1.value].answers[2]
+  answerquestion4.textContent = questions[rna1.value].answers[3]
+  valuecheck1.value = questions[rna1.value].answers[0];
+  valuecheck2.value = questions[rna1.value].answers[1];
+  valuecheck3.value = questions[rna1.value].answers[2];
+  valuecheck4.value = questions[rna1.value].answers[3];
+  var compare = thevalueofcheckedboxs();
+return questions[rna1.value -1].correctAnswer;
 }
-console.log(questions[1].answers[1])
-function turn1(){
 
+function submit()
+{ 
+  console.log(thevalueofcheckedboxs());
+}
+
+
+
+
+
+
+
+var myvalue;
+function thevalueofcheckedboxs()
+{
+  let checkboxes = [valuecheck1, valuecheck2, valuecheck3, valuecheck4]
+  checkboxes.forEach(box=>{box.onclick = e =>  myvalue= e.target.value})
+  console.log(myvalue)
+ return myvalue
+}
+
+
+
+
+function initilizethe()
+{
+  valuecheck1.checked = false;
+  valuecheck2.checked = false;
+  valuecheck3.checked = false;
+  valuecheck4.checked = false;
+}
+
+function StepForward(lol )
+{
+  questiontitle.textContent = questions[lol.value].question;
+  answerquestion1.textContent = questions[lol.value].answers[0]
+  answerquestion2.textContent = questions[lol.value].answers[1]
+  answerquestion3.textContent = questions[lol.value].answers[2]
+  answerquestion4.textContent = questions[lol.value].answers[3]
+}
+  
+
+
+
+function turn1(){
 if(document.getElementById('username').value)
 {
-
 step.style.backgroundColor ='green',
 document.getElementById('title').textContent = "Welcome" + "  " + document.getElementById('username').value;
 document.getElementById('username').style.display = "none";
 document.getElementById('username').value = "";
 document.getElementById('btn').innerHTML = " proceed to the next step"
 document.getElementById('svgdis').style.display = "none";
-document.getElementById('displayQA').style.display ="block"; 
-
+document.getElementById('displayQA2').style.display = "block"
 }else if(step.style.backgroundColor =='green')
 {
+      passTheNextquestion();
       step2.style.backgroundColor = 'green';
       document.getElementById('btn').style.display = "none";
+      document.getElementById('displayQA2').style.display = "none"
+      document.getElementById('displayQA').style.display ="block"; 
+    
 }
 else if( document.getElementById('btn').style.display == "none" && step2.style.backgroundColor == 'green')
 {
       alert('happy to see you ')
-      document.getElementById('spacediv3').style.backgroundColor = 'green'
-      console.log(document.getElementById('spacediv3'));
+      
 }else{
       alert('insert you name please');
       }
 }
-
-
-
-// function pageelem(parent,t,html,c)
-// {
-//       const ele = document.createElement(t);
-//       ele.innerHTML = html;
-//       ele.classList.add(c);
-//       return parent.appendChild(ele);
-// }
-
-// function loadData ()
-// {
-//  fetch(url).then(res =>res.json()).then(data=>{console.log(data)})
-// }
-
-
 
 
 
